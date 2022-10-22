2be1d48795f6207ab3cac47939ad93f337864e0b
Natalya <nemia.me@gmail.com>
2022-10-22 15:00:25 +0300

    Add 008 009 Massiv

---
+++ Example008_Massiv/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 9, 10, 0, 8, 5, 3, 1, 0 };
+int find = 0;
+int i = 0;
+//int len = 8;
+bool res = false;
+
+
+while (i < array.Length)
+{
+    if (find == array[i])
+    {
+        Console.WriteLine($"Ура! Искомое значение {find} найдено в под индексом {i}!");
+        res = true;
+    }
+    i++;
+}
+
+if (res == false) Console.WriteLine("Не нашлось :'(");
---
+++ Example009_MassivPRO/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillArray(int[] collection)
+{
+    int i = 0;
+    while (i < collection.Length)
+    {
+        collection[i] = new Random().Next(0, 10);
+        i++;
+    }
+    //return collection;
+}
+
+void PrintArray(int[] collection)
+{
+    int i = 0;
+    while (i < collection.Length)
+    {
+        Console.WriteLine(collection[i]);
+        i++;
+    }
+}
+
+int FindPosition(int find, int[] collection)
+{
+    int i = 0;
+    int position = -1;
+    while (i < collection.Length)
+    {
+        if (collection[i] == find)
+        {
+            position = i;
+            break;
+        }
+        i++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = FindPosition(77, array);
+Console.WriteLine(pos);
